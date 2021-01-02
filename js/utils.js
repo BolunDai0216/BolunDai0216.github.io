ed8b4f5234b0f120cd6e1efc2268b8687ca6933c
Bolun Dai <bdai@andrew.cmu.edu>
2021-01-02 13:04:56 -0500

    added underline to nav links other than the index page

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,11 @@ function changeBackgroundColor(color) {
     body = document.getElementsByTagName('body');
     body[0].style.backgroundColor = color;
 }
+
+function addUnderline(object) {
+    object.style.textDecoration = "underline";
+}
+
+function removeUnderline(object) {
+    object.style.textDecoration = "none";
+}

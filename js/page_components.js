7b87531fe9454bd7a9e195f947094191f9b6828a
Bolun Dai <bdai@andrew.cmu.edu>
2021-02-01 19:13:35 -0500

    updated line search blog

--- js/page_components.js
+++ js/page_components.js
@@ -5,8 +5,8 @@ Vue.component('my-footer', {
                            <p class="footer-p col-6">&copy Bolun Dai 2021</p> \
                            <p class="col-6 text-end"> \
                            <a href="mailto:bd1555@nyu.edu" style="padding: 0px 0px 0px 2%;"> \
-                               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class= "bi bi-envelope" viewBox="0 0 16 16"> \
-                                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/> \
+                               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-envelope-fill" viewBox="0 0 16 16"> \
+                                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/> \
                                </svg> \
                            </a> \
                            <a href="https://www.linkedin.com/in/bolun-dai" style="padding: 0px 0px 0px 2%;"> \
